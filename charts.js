414becf11da198a8d3f56b2022e225a325a8d309
Afton Snider <asnider@live.com>
2022-09-18 10:12:52 -0500

    Added displays

---
+++ charts.js
@@ -0,0 +1,204 @@
+function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+
+  // Use the list of sample names to populate the select options
+  d3.json("samples.json").then((data) => {
+    var sampleNames = data.names;
+
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+
+    // Use the first sample from the list to build the initial plots
+    var firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+}
+
+// Initialize the dashboard
+init();
+
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  buildMetadata(newSample);
+  buildCharts(newSample);
+  
+}
+
+// Demographics Panel 
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    // Filter the data for the object with the desired sample number
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    // Use d3 to select the panel with id of `#sample-metadata`
+    var PANEL = d3.select("#sample-metadata");
+
+    // Use `.html("") to clear any existing metadata
+    PANEL.html("");
+
+    // Use `Object.entries` to add each key and value pair to the panel
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // tags for each key-value in the metadata.
+    Object.entries(result).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
+
+  });
+}
+
+// 1. Create the buildCharts function.
+function buildCharts(sample) {
+  // 2. Use d3.json to load and retrieve the samples.json file 
+  d3.json("samples.json").then((data) => {
+    // 3. Create a variable that holds the samples array. 
+    var sampleData = data.samples;
+
+    var metaData = data.metadata;
+    // 4. Create a variable that filters the samples for the object with the desired sample number.
+    var filteredData = sampleData.filter(sam => sam.id ==sample);
+
+    //  5. Create a variable that holds the first sample in the array.
+    var firstSample = filteredData[0];
+
+    // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
+    var otu_ids = firstSample.otu_ids;
+    var otu_labels = firstSample.out_labels;
+    var sample_values = firstSample.sample_values;
+
+    
+    // 7. Create the yticks for the bar chart.
+    // Hint: Get the the top 10 otu_ids and map them in descending order  
+    //  so the otu_ids with the most bacteria are last. 
+  
+    var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+
+    // 8. Create the trace for the bar chart. 
+    var trace = {
+    
+      y: yticks,
+      x: sample_values.slice(0, 10).reverse(),
+      text: otu_labels,
+      // text: otu_labels.slice(0, 10).reverse(),
+      type: "bar",
+      orientation: "h"
+      
+    };
+
+    var barData = [trace]
+
+    // 9. Create the layout for the bar chart. 
+    var barLayout = {
+      title: "Top 10 Bacteria Cultures Found",
+      margin: {
+        l: 100,
+        r: 100,
+        t: 100,
+        b: 100 
+    }
+
+    };
+    
+    // 10. Use Plotly to plot the data with the layout. 
+    Plotly.newPlot("bar", barData, barLayout);
+ // });
+//};
+
+//Deliverable 2: Bubble Chart
+
+    // 1. Create the trace for the bubble chart.
+  
+      var trace2 = {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+       mode: 'markers',
+       marker: {
+        color: otu_ids,
+        colorscale: "sunset",
+        size: sample_values
+      }
+      
+        
+      };
+      var bubbleData = [trace2];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: "Bacteria Species per Sample",
+      hovermode: "closest",
+      xaxis: {title: "OTU ID"},
+      margin: {
+        l: 100,
+        r: 100,
+        t: 100,
+        b: 100 
+    }
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
+
+    // Deliverable 3: Gauge Chart
+    var metadata = data.metadata;
+
+    // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    var resultArray2 = metadata.filter(sampleObj => sampleObj.id == sample);
+
+// 2. Create a variable that holds the first sample in the metadata array.
+    var result2 = resultArray2[0];
+
+    // Create a variale to hold the washing frequency.
+    var washingFrequency = result2.washingFrequency
+
+    // 4. Create the trace for the gauge chart.
+    var gaugeData = [ {
+      
+        domain: { x: [0, 1], y: [0, 1] },
+        value: washingFrequency,
+        title: { text: "<b> Belly Button Washing Frequency</b> <br> Scrubs per Week"},
+        type: "indicator",
+        mode: "gauge+number",
+        delta: { reference: 380 },
+        gauge: {
+          axis: { range: [null, 10] },
+          bar: {color: "black"},
+          steps: [
+            { range: [0, 2], color: "green" },
+            { range: [2, 4], color: "pink" },
+            { range: [4, 6], color: "orchid" },
+            { range: [6, 8], color: "navy" },
+            { range: [8, 10], color: "cyan" }
+                    ],
+          threshold: {
+            line: { color: "black", width: 4 },
+            thickness: 0.75,
+            value: 490
+          }
+        }
+      }
+
+   
+  ];
+
+      
+     
+ 
+    
+    // 5. Create the layout for the gauge chart.
+    var gaugeLayout = { 
+      width: 600, height: 450, margin: { t: 0, b: 0 }
+
+    };
+
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot('gauge', gaugeData, gaugeLayout);
+  
+  });
+}
--- plots.js
+++ plots.js
@@ -1,3 +1,103 @@
 d3.json("samples.json").then(function(data){
-    console.log("hello");
+    console.log(data);
 });
+
+// display results in descending order and take out null values
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person => person.wfreq).sort((a,b) => b - a);
+    filteredWfreq = wfreq.filter(element => element != null);
+    console.log(filteredWfreq);
+});
+
+// print each key-value pair
+// console.log(Object.entries(reasearcher1));
+
+// print to the console each train and corresponding property
+// reasearcher1.forEach(([first, second]) => console.log(first + ":" + second));
+
+// skill drill Print all the metadata of the first person in the samples.json() dataset(ID 940)
+d3.json("samples.json").then(function(data){
+    firstPerson = data.metadata[0];
+    Object.entries(firstPerson).forEach(([key, value]) =>
+      {console.log(key + ': ' + value);});
+});
+
+// function init() {
+//     data = [{
+//       x: [1, 2, 3, 4, 5],
+//       y: [1, 2, 4, 8, 16] 
+//     }];
+//     Plotly.newPlot("plot", data);
+//   };
+  
+//   d3.selectAll("#dropdownMenu").on("change", updatePlotly);
+//   function updatePlotly() {
+//     var dropdownMenu = d3.select("#dropdownMenu");
+//     var dataset = dropdownMenu.property("value");
+  
+//     var xData = [1, 2, 3, 4, 5];
+//     var yData = [];
+  
+//     if (dataset === 'dataset1') {
+//       yData = [1, 2, 4, 8, 16];
+//     };
+  
+//     if (dataset === 'dataset2') {
+//       yData = [1, 10, 100, 1000, 10000];
+//     };
+  
+//     var trace = {
+//       x: [xData],
+//       y: [yData],
+//     };
+//     Plotly.restyle("plot", trace);
+//   };
+  
+//   init();
+
+
+// function to create a dropdown menu of ID #s dynamnically.
+  function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
+  
+  init();
+
+  // optionChanged function  that is called from html
+  // tasks need to be modularized(seperate)
+  function optionChanged(newSample) {
+    buildMetadata(newSample);
+    buildCharts(newSample);
+  };
+
+  //declare buildMetadata function
+  function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      var PANEL = d3.select("#sample-metadata");
+  
+      PANEL.html("");
+      PANEL.append("h6").text("ID: " + result.id);
+      PANEL.append("h6").text("Ethnicity: " + result.ethnicity);
+      PANEL.append("h6").text("Gender: " + result.gender);
+      PANEL.append("h6").text("Age: " + result.age);
+      PANEL.append("h6").text("Location: " + result.location);
+      PANEL.append("h6").text("BBTYPE: " + result.bbtype);
+      PANEL.append("h6").text("WFREQ: " + result.wfreq);
+    });
+  };
+
+
+
